000d1a6f707397b167effa3884f8595b04903a43
lilinzeze <18256061311@163.com>
2018-09-07 18:32:50 +0800

    空一行

--- juejin-common/src/main/java/com/qhcs/juejin/common/dao/BaseDao.java
+++ juejin-common/src/main/java/com/qhcs/juejin/common/dao/BaseDao.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright (C) 2017 ShenZhen QiHangChuangShi Co.,Ltd All Rights Reserved.
  * 未经本公司正式书面同意，其他任何个人、团体不得使用、复制、修改或发布本软件.
  * 版权所有深圳市起航创势科技有限公司 www.qihcs.com.
@@ -20,6 +20,7 @@ import com.qhcs.juejin.common.page.Page;
  * @author zhuwenbin
  */
 public interface BaseDao<T> {
+ 
 
 	/** 插入新对象 */
 	int insert(T o);
